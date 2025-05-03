5919640d7030a439dcb402029a0fd1146296ab37
villyjayt <acekira100@gmail.com>
2025-05-03 09:54:48 +0800

    This commit is for adding a mini-project for GPIO toggling: 1. GPIO pin used here is Pin 17. This repo has the updates from update-linux-gpio-driver branch 2. This mini-project only turns the LED ON and OFF

---
+++ projects/temp-triggered-fan/src/platform/linux/no-lgpio-use.c
@@ -0,0 +1,33 @@
+#include <linux/gpio.h>
+#include "no_os_gpio.h"
+#include "parameters.h"
+#include <stdio.h>
+int main() {
+    int pin = 17; // Pin number is not straightforward 
+    
+    
+    // Acquire file descriptior for pin 
+    struct no_os_gpio_desc *descriptor;
+    struct no_os_gpio_init_param param = {
+        .port = 0,
+        .number = pin,
+        .pull = NO_OS_PULL_NONE,
+        .platform_ops = GPIO_OPS,
+        .extra = NULL 
+        };
+    
+    no_os_gpio_get(&descriptor, &param); 
+    
+    // Set GPIO output to HIGH
+    no_os_gpio_direction_output(descriptor, NO_OS_GPIO_HIGH);
+    sleep(1);
+    
+    // Set GPIO output to LOW
+    no_os_gpio_direction_output(descriptor, NO_OS_GPIO_LOW);
+    sleep(1);
+    
+    //Close descriptor
+    no_os_gpio_remove(descriptor);
+    
+    return 0;
+}
